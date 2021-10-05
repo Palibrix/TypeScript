9addaa4239f113247c7d7cab816cf317ad56bdb3
Palibrix <90177716+Palibrix@users.noreply.github.com>
2021-10-05 12:48:16 +0300

    Update AgeCalculator.js

--- AgeCalculator.js
+++ AgeCalculator.js
@@ -1,23 +1,27 @@
 "use strict";
 exports.__esModule = true;
-var readline = require("readline");
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+var rl = require("readline-sync");
 var birth = "";
 var today = "";
-var stdin = process.openStdin();
-rl.question('Введіть вашу дату народження та сьогоднішню дату через пробіл(dd.mm.yyyy dd.mm.yyyy)', function (date) {
-    var splitted = date.split(' ', 2);
-    birth = splitted[0];
-    today = splitted[1];
-    var birthSp = birth.split('.', 3);
-    var todaySp = today.split('.', 3);
-    var months = +todaySp[2] * +12;
-    months += +todaySp[1];
-    months -= +birthSp[2] * 12;
-    months -= +birthSp[1];
-    console.log("Вам " + months + " місяць(ів)");
-    rl.close();
-});
+var date = rl.question('How old R U? Type date of birthday and todays(dd.mm.yyyy dd.mm.yyyy)');
+// rl.question('Введіть вашу дату народження та сьогоднішню дату через пробіл(dd.mm.yyyy dd.mm.yyyy)', (date)=>{
+var splitted = date.split(' ', 2);
+birth = splitted[0];
+today = splitted[1];
+var birthSp = birth.split('.', 3);
+var todaySp = today.split('.', 3);
+var tYear = +todaySp[2];
+var tMonths = +todaySp[1];
+var bYear = +birthSp[2];
+var bMonths = +birthSp[1];
+// let months:number;
+cons(math(tYear, tMonths, bYear, bMonths));
+function math(tYear, tMonth, bYear, bMonths) {
+    var months = (tYear * 12 + tMonth) - bYear * 12 - bMonths;
+    return months;
+}
+function cons(months) {
+    console.log("U R " + months + " months old");
+}
+// rl.close();
+// });
